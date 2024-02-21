d75c1ce1b4ca3d4e235ad1d366e93e3c789b065e
JianingC0913 <129560784+JianingC0913@users.noreply.github.com>
2024-02-21 00:20:43 -0500

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = "login"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login", views.login, name="login"),
     path("auth_home/", views.auth_home, name="auth_home"),
 ]
