df97417211e358d4fc745744802bd738813d9dc7
nicoloschianchi <wfh3xg@virginia.edu>
2024-03-12 18:12:52 -0400

    Wrote skeleton code for first test case for uploading txt file to Amazon S3

--- file_upload/tests.py
+++ file_upload/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import boto3
 
-# Create your tests here.
+class FileUploadTest(TestCase):
+    def upload_txt_file(self):
+        # constants
+        BUCKET_NAME = 'YOUR-BUCKET-NAME’
+        S3_FILE = 'YOUR-MANUALLY-UPLOADED-FILE-NAME'
+        LOCAL_NAME = 'YOUR-LOCAL-NAME-FOR-THIS-FILE'
+
+        s3 = boto3.resource('s3')
+
+        # test listing
+        bucket = s3.Bucket(BUCKET_NAME)
+        for f in bucket.objects.all():
+            print(f.key)
+
+        # test downloading
+        bucket.download_file(S3_FILE, LOCAL_NAME)
+
+        # test uploading
+        data = open('YOUR-LOCAL-FILE', 'rb')
+        bucket.put_object(Key='YOUR-FILE-NAME-ON-S3-FOR-THIS-FILE', Body=data)
+
