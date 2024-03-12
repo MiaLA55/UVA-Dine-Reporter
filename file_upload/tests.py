1c3216b49cceed6622605ee1f3671d85829fdeab
Adityak132004 <adityarkakkar13@gmail.com>
2024-03-12 18:18:49 -0400

    added two test methods

--- file_upload/tests.py
+++ file_upload/tests.py
@@ -22,3 +22,41 @@ class FileUploadTest(TestCase):
         data = open('YOUR-LOCAL-FILE', 'rb')
         bucket.put_object(Key='YOUR-FILE-NAME-ON-S3-FOR-THIS-FILE', Body=data)
 
+    def upload_pdf_file(self):
+        BUCKET_NAME = 'YOUR-BUCKET-NAME’
+        S3_FILE = 'YOUR-MANUALLY-UPLOADED-FILE-NAME'
+        LOCAL_NAME = 'YOUR-LOCAL-NAME-FOR-THIS-FILE'
+
+        s3 = boto3.resource('s3')
+
+        # test listing
+        bucket = s3.Bucket(BUCKET_NAME)
+        for f in bucket.objects.all():
+            print(f.key)
+
+        # test downloading
+        bucket.download_file(S3_FILE, LOCAL_NAME)
+
+        # test uploading
+        data = open('YOUR-LOCAL-FILE', 'rb')
+        bucket.put_object(Key='YOUR-FILE-NAME-ON-S3-FOR-THIS-FILE', Body=data)
+
+    def upload_jpg_file(self):
+        BUCKET_NAME = 'YOUR-BUCKET-NAME’
+        S3_FILE = 'YOUR-MANUALLY-UPLOADED-FILE-NAME'
+        LOCAL_NAME = 'YOUR-LOCAL-NAME-FOR-THIS-FILE'
+
+        s3 = boto3.resource('s3')
+
+        # test listing
+        bucket = s3.Bucket(BUCKET_NAME)
+        for f in bucket.objects.all():
+            print(f.key)
+
+        # test downloading
+        bucket.download_file(S3_FILE, LOCAL_NAME)
+
+        # test uploading
+        data = open('YOUR-LOCAL-FILE', 'rb')
+        bucket.put_object(Key='YOUR-FILE-NAME-ON-S3-FOR-THIS-FILE', Body=data)
+
