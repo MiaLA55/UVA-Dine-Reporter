37be12dfae3b7375fcd91f89ca0f0b580a9eba47
JianingC0913 <129560784+JianingC0913@users.noreply.github.com>
2024-03-16 22:15:21 -0400

    Update tests.py

--- file_upload/tests.py
+++ file_upload/tests.py
@@ -4,16 +4,22 @@ from django.conf import settings
 from django.test import TestCase
 import boto3
 
+AWS_ACCESS_KEY_ID = "AKIAU6GD2ERXH4XMKEH5"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "dininghallapp"
+
 
 class FileUploadTest(TestCase):
     def test_upload_txt_file(self):
-        s3 = boto3.client('s3',
-                          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        s3.upload_file("file_upload/test.txt", settings.AWS_STORAGE_BUCKET_NAME, "test.txt")
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        s3.upload_file("file_upload/test.txt", AWS_STORAGE_BUCKET_NAME, "test.txt")
 
         # test downloading
-        s3.download_file(settings.AWS_STORAGE_BUCKET_NAME, "test.txt", "test_passed.txt")
+        s3.download_file(AWS_STORAGE_BUCKET_NAME, "test.txt", "test_passed.txt")
 
         # Assertions for successful download
         self.assertTrue(os.path.isfile("test_passed.txt"), "Downloaded file not found")
@@ -23,13 +29,15 @@ class FileUploadTest(TestCase):
             os.remove("test_passed.txt")
 
     def test_upload_pdf_file(self):
-        s3 = boto3.client('s3',
-                          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        s3.upload_file("file_upload/test.pdf", settings.AWS_STORAGE_BUCKET_NAME, "test.pdf")
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        s3.upload_file("file_upload/test.pdf", AWS_STORAGE_BUCKET_NAME, "test.pdf")
 
         # test downloading
-        s3.download_file(settings.AWS_STORAGE_BUCKET_NAME, "test.pdf", "test_passed.pdf")
+        s3.download_file(AWS_STORAGE_BUCKET_NAME, "test.pdf", "test_passed.pdf")
 
         # Assertions for successful download
         self.assertTrue(os.path.isfile("test_passed.pdf"), "Downloaded file not found")
@@ -39,13 +47,15 @@ class FileUploadTest(TestCase):
             os.remove("test_passed.pdf")
 
     def test_upload_jpg_file(self):
-        s3 = boto3.client('s3',
-                          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        s3.upload_file("file_upload/test.jpg", settings.AWS_STORAGE_BUCKET_NAME, "test.jpg")
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        s3.upload_file("file_upload/test.jpg", AWS_STORAGE_BUCKET_NAME, "test.jpg")
 
         # test downloading
-        s3.download_file(settings.AWS_STORAGE_BUCKET_NAME, "test.jpg", "test_passed.jpg")
+        s3.download_file(AWS_STORAGE_BUCKET_NAME, "test.jpg", "test_passed.jpg")
 
         # Assertions for successful download
         self.assertTrue(os.path.isfile("test_passed.jpg"), "Downloaded file not found")
