492f267df1dd7cf6b7b6a572b06466397e67e5b6
Mia Ark <miaark952@gmail.com>
2024-04-23 19:20:45 -0400

    allauth temps

--- diningHallApp/settings.py
+++ diningHallApp/settings.py
@@ -35,10 +35,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
+    "allauth",
     "bootstrap5",
     "storages",
 ]
@@ -59,7 +59,7 @@ ROOT_URLCONF = "diningHallApp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates"),],
+        "DIRS": [os.path.join(BASE_DIR, "../templates"), ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
