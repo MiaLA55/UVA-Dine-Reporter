f465371aac189cd48280ee3ab1b647b0dfdee808
Mia Ark <miaark952@gmail.com>
2024-04-23 19:56:40 -0400

    restart all

--- diningHallApp/settings.py
+++ diningHallApp/settings.py
@@ -35,10 +35,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
-    "allauth",
     "bootstrap5",
     "storages",
 ]
