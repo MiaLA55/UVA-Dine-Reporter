aef9c374d058085f3a8f374ecca4b3c5fbcc8499
Mia Ark <miaark952@gmail.com>
2024-04-24 15:47:41 -0400

    small spelling fix

--- login/views.py
+++ login/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from file_upload.models import Report, Tag
 from django.urls import reverse
 import boto3
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -12,8 +13,6 @@ from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
-from django.utils import timezone
-from datetime import datetime
 
 
 AWS_ACCESS_KEY_ID = "AKIAU6GD2ERXH4XMKEH5"
