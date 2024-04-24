a81a158f458c4d10a534aa1268fc39f9ad64aa05
Mia Ark <miaark952@gmail.com>
2024-04-23 21:26:00 -0400

    base allauth customized

--- login/form.py
+++ login/form.py
@@ -4,7 +4,6 @@ from django import forms
 # class FileUploadForm(forms.Form):
 #     filename = forms.CharField(widge=forms.HiddenInput())
 
-
 class ResolveReportForm(forms.Form):
     report_name = forms.CharField(label="Report Name", max_length=100)
     resolve_notes = forms.CharField(label="Resolve Notes", widget=forms.Textarea)
